8a7b1dae5d1b3e75ae8f34a7f0f36d546735ef30
Thiago Lagden <lagden@gmail.com>
2017-07-19 17:54:31 -0300

    fix #1 replacements logic

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
-/* eslint max-depth: 0 */
 'use strict'
 
 import {encode} from 'lagden-hex'
 
-function highlight(source, q, tpl = '<mark>$&</mark>', split = true) {
-	const spade = '\u2664'
-	let words = (split ? q.split(' ') : [q]).sort((a, b) => b.length - a.length)
-	let map = {}
-	let unique = {}
-	let cc = 0
-	for (let i = 0; i < words.length; i++) {
-		const word = words[i]
-		if (word) {
-			const matches = source.match(new RegExp(`(${encode(word)})`, 'ig'))
-			if (matches) {
-				for (let k = 0; k < matches.length; k++) {
-					const match = matches[k]
-					if (unique[match]) {
-						continue
-					}
-					unique[match] = 1
-					const n = `${spade}${cc}`
-					map[n] = match
-					source = source.replace(new RegExp(`(${encode(match)})`, 'g'), n)
-					cc++
-				}
-			}
-		}
+function onlyUnique(value, index, arr) {
+	return arr.indexOf(value) === index && value
+}
+
+function highlight(...args) {
+	let [source, q, tpl = '<mark>$&</mark>', split = true] = args
+	if (typeof tpl === 'boolean') {
+		split = tpl
+		tpl = '<mark>$&</mark>'
 	}
-	const regex = new RegExp(`(${spade}[0-9+])`, 'g')
-	const result = source.replace(regex, m => m.replace(m, tpl).replace(m, map[m]))
-	words = null
-	map = null
-	unique = null
-	return result
+
+	const words = (split ? q.split(' ') : [q])
+		.map(word => word.trim())
+		.filter(onlyUnique)
+		.sort((a, b) => b.length - a.length)
+		.map(word => encode(word))
+
+	const regex = new RegExp(`${words.join('|')}`, 'g')
+	return source.replace(regex, tpl)
 }
 
 export default highlight
